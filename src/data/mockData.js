bc73a1330443c3c15a3e0093d84654cc9833a852
rahuldaivadnya <rahudaivadnya@gmail.com>
2025-11-17 17:10:00 +0530

    officer section updated

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -342,7 +342,7 @@ export const mockData = {
     },
     {
       id: 1,
-      name: "श्री मोरे रंजीत सरजेऱाव ",
+      name: "श्री. रणजीत संजय मोरे",
       designation: "सरपंच, ग्रामपंचायत, ढोलेवाडी",
       department: "Administration",
       phone: "+91-937-3356931",
